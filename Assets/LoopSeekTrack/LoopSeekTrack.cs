2130d8dcecb832241e9b9bfe8faf34d19f867937
yano <yano_123@hotmail.com>
2018-02-10 20:25:28 +0900

    initial commit

---
+++ Assets/LoopSeekTrack/LoopSeekTrack.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+[TrackColor (0.0f, 1.0f, 1.0f)]
+[TrackClipType (typeof(LoopSeekClip))]
+public class LoopSeekTrack : TrackAsset
+{
+    public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)
+    {
+        var scriptPlayable = ScriptPlayable<LoopSeekBehaviour>.Create(graph, inputCount);
+
+        //LoopSeekBehaviour b = scriptPlayable.GetBehaviour();
+
+        SingletonLoopSeek.Instance.ClearDic();
+
+        //This foreach will rename clips based on what they do, and collect the markers and put them into a dictionary
+        //Since this happens when you enter Preview or Play mode, the object holding the Timeline must be enabled or you won't see any change in names
+        foreach (var c in GetClips())
+        {
+            LoopSeekClip clip = (LoopSeekClip)c.asset;
+            string clipName = c.displayName;
+
+            if (clip.label == clip.label_next)
+            {
+                clipName = "●(" + clip.label.ToString() + ',' + clip.label_next.ToString() + ")";
+            }
+            else
+            {
+                clipName = "↩︎(" + clip.label.ToString() + ',' + clip.label_next.ToString() + ")";
+            }
+
+            // c.startをSingletonに追加する
+            SingletonLoopSeek.Instance.AddLabelTime(clip.label, c.start);
+
+            c.displayName = clipName;
+        }
+
+        return scriptPlayable;
+    }
+}
---
+++ Assets/LoopSeekTrack/SingletonLoopSeek.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using AdvancedInspector;
+
+[RequireComponent(typeof(PlayableDirector))]
+public class SingletonLoopSeek : SingletonLoopSeekBase<SingletonLoopSeek>
+{
+    PlayableDirector playableDirector;
+
+    [SerializeField]
+    Dictionary<int, double> dicLabelTime = new Dictionary<int, double>();
+
+    bool canSetTime = true;
+
+    void Awake()
+    {
+        playableDirector = GetComponent<PlayableDirector>();
+    }
+
+    void Start() { }
+
+    public void ClearDic()
+    {
+        dicLabelTime.Clear();
+    }
+
+    public void AddLabelTime(int label, double time)
+    {
+        dicLabelTime.Add(label, time);
+    }
+
+    public void ErrorCheck(int label)
+    {
+        if (!dicLabelTime.ContainsKey(label))
+            Debug.LogError("dicLabelTime does not contain label:" + label.ToString() + " !", transform);
+    }
+
+    // TimelineのLoopSeekBehaviourからの呼び出しの場合trueを指定する。
+    // それ以外の場合は
+    public void SetTime(int label, bool fromTimeline = false)
+    {
+        if (fromTimeline)
+        {
+            if (canSetTime)
+            {
+                ErrorCheck(label);
+                playableDirector.time = dicLabelTime[label];
+            }
+        }
+        else
+        {
+            StartCoroutine("SetTimeCoroutine", label);
+        }
+    }
+
+    IEnumerator SetTimeCoroutine(int _label)
+    {
+        ErrorCheck(_label);
+
+        canSetTime = false;
+
+        playableDirector.time = dicLabelTime[_label];
+
+        yield return new WaitForSeconds(0.1f);
+
+        canSetTime = true;
+    }
+
+    [Inspect(0), Title(FontStyle.Bold, "Test"), Spacing(Before = 3)]
+    int testLabel;
+
+    [Inspect(1)]
+    void Test()
+    {
+        StartCoroutine("SetTimeCoroutine", testLabel);
+    }
+}
